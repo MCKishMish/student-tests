e3968bfcefa841b50efd890cc52e656d978815be
MCKishMish <pashabest032@gmail.com>
2023-03-05 01:52:54 +0300

    waiting task is done

--- src/test/java/study/stepup/UITests.java
+++ src/test/java/study/stepup/UITests.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,18 +13,30 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.with;
 
 public class UITests {
-    WebDriver wd;
+    private WebDriver wd;
+    private WebDriverWait webDriverWait;
+
+    public static void waitVisibleElement(WebElement we) {
+        with().pollDelay(100, TimeUnit.MILLISECONDS).await().atMost
+                (90, TimeUnit.SECONDS).until(we::isDisplayed);
+    }
 
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "/Users/shalatonov/Downloads/chromedriver_mac64/chromedriver");
         wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wd.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        webDriverWait=new WebDriverWait(wd,Duration.ofSeconds(10));
     }
 
     @Test
-    public void pikabuCheck() throws InterruptedException {
+    public void pikabuCheck() {
 
         wd.get("https://pikabu.ru/");
 
@@ -45,9 +58,33 @@ public class UITests {
         wd.findElement(passwordInputInModal).sendKeys("Qwerty");
         wd.findElement(loginButtonInModal).click();
 
-        WebElement loginError = new WebDriverWait(wd, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Ошибка. Вы ввели неверные данные авторизации']")));
-        Assert.assertTrue(loginError.isDisplayed());
+        Assert.assertTrue(wd.findElement(By.xpath("//span[text()='Ошибка. Вы ввели неверные данные авторизации']")).isDisplayed());
+    }
+
+    @Test
+    public void googleSearchPobeda() {
+
+        wd.get("https://google.com/");
+        By searchInput = By.cssSelector("[title='Поиск']");
+        wd.findElement(searchInput).sendKeys("Сайт компании Победа");
+        wd.findElement(searchInput).sendKeys(Keys.ENTER);
+
+        By pobedaLink = By.cssSelector("h3");
+        wd.findElement(pobedaLink).click();
+
+        WebElement goToKaliningrad = wd.findElement(By.cssSelector("[alt='banner_pobedanew_winter_kaliningrad.jpeg']"));
+        waitVisibleElement(goToKaliningrad);
+        Assert.assertTrue(wd.findElement(By.xpath("//img[@alt='banner_pobedanew_winter_kaliningrad.jpeg']/following-sibling::div[3]/div/div")).getText().equals("Полетели в Калининград!"));
+
+        wd.findElement(By.xpath("//div[text()='РУС']")).click();
+        wd.findElement(By.xpath("//div[text()='English']")).click();
+        By ticketSearch = By.xpath("//div[@class=\"__mantine-ref-text dp-vbfp0a\"][contains(text(),'Ticket search')]");
+        By onlineCheckIn = By.xpath("//div[@class=\"__mantine-ref-text dp-vbfp0a\"][contains(text(),'Online check-in')]");
+        By manageMyBooking = By.xpath("//div[@class=\"__mantine-ref-text dp-vbfp0a\"][contains(text(),'Manage my booking')]");
+
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(wd.findElement(ticketSearch),"Ticket search"));
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(wd.findElement(onlineCheckIn),"Online check-in"));
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(wd.findElement(manageMyBooking),"Manage my booking"));
     }
 
     @After
