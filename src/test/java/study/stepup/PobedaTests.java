bea3475af85c4343ee45c8e5cf3512ca3569e526
MCKishMish <pashabest032@gmail.com>
2023-03-08 01:31:39 +0300

    3d task pobeda test is done

--- src/test/java/study/stepup/PobedaTests.java
+++ src/test/java/study/stepup/PobedaTests.java
@@ -1,16 +1,10 @@
 package study.stepup;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import study.stepup.pages.PobedaHomePage;
 
 import java.time.Duration;
@@ -26,7 +20,6 @@ public class PobedaTests {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        //webDriverWait=new WebDriverWait(wd,Duration.ofSeconds(10));
     }
 
     @Test
@@ -38,12 +31,20 @@ public class PobedaTests {
     }
 
     @Test
-    public void pobeda2Test() throws InterruptedException {
+    public void pobeda2Test() {
         PobedaHomePage pobedaHomePage = new PobedaHomePage(driver);
         pobedaHomePage.checkTitle("Авиакомпания «Победа» - купить билеты на самолёт дешево онлайн, прямые и трансферные рейсы");
         pobedaHomePage.checkLogo();
         pobedaHomePage.checkTicketSearchSection();
         pobedaHomePage.tryFindingTicketsWithoutReqParams();
+    }
+
+    @Test
+    public void pobeda3Test() throws InterruptedException {
+        PobedaHomePage pobedaHomePage = new PobedaHomePage(driver);
+        pobedaHomePage.checkTitle("Авиакомпания «Победа» - купить билеты на самолёт дешево онлайн, прямые и трансферные рейсы");
+        pobedaHomePage.checkLogo();
+        pobedaHomePage.bookingSearch();
     }
 
 
--- src/test/java/study/stepup/pages/PobedaHomePage.java
+++ src/test/java/study/stepup/pages/PobedaHomePage.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.with;
 
 public class PobedaHomePage {
 
@@ -43,10 +47,30 @@ public class PobedaHomePage {
     @FindBy(xpath = "//div[text()='Поиск']")
     private WebElement ticketSearchButton;
 
+    @FindBy(xpath = "//div[@class='__mantine-ref-text dp-vbfp0a' and text()='Управление бронированием']")
+    private WebElement bookingManagementTab;
+
+    @FindBy(css = "[id=':r3:']")
+    private WebElement clientLastname;
+
+    @FindBy(css = "[id=':r4:']")
+    private WebElement orderNumber;
+
+    @FindBy(css = "[class='dp-ujv363']")
+    private WebElement bookingSearchButton;
+
+    @FindBy(xpath = "//div[text()='Заказ с указанными параметрами не найден']")
+    private WebElement bookingSearchErrorMessage;
+
     public PobedaHomePage (WebDriver driver) {
         this.driver=driver;
         driver.get("https://pobeda.aero/");
         PageFactory.initElements(driver, this);
+    }
+
+    public static void waitVisibleElement(WebElement we) {
+        with().pollDelay(100, TimeUnit.MILLISECONDS).await().atMost
+                (90, TimeUnit.SECONDS).until(we::isDisplayed);
     }
 
     public void checkTitle(String title) {
@@ -81,5 +105,17 @@ public class PobedaHomePage {
         finishPoint.sendKeys(Keys.ENTER);
         ticketSearchButton.click();
         Assert.assertTrue(dateTo.getCssValue("color").equals("rgba(185, 0, 85, 1)"));
+    }
+
+    public void bookingSearch () throws InterruptedException {
+        bookingManagementTab.click();
+        Assert.assertTrue(clientLastname.isDisplayed());
+        Assert.assertTrue(orderNumber.isDisplayed());
+        orderNumber.sendKeys("XXXXXX");
+        clientLastname.sendKeys("Qwerty");
+        bookingSearchButton.click();
+        ArrayList<String> tabs2 = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        Assert.assertTrue(bookingSearchErrorMessage.isDisplayed());
     }
 }
